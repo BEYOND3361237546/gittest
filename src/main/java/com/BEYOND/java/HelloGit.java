22fa4908c80d54cfc1fb88e29f6af05a6012de23
BEYOND <3361237546@qq.com>
2022-05-29 15:56:22 +0800

    first commit

---
+++ src/main/java/com/BEYOND/java/HelloGit.java
@@ -0,0 +1,11 @@
+package com.BEYOND.java;
+
+/**
+ * @author BEYOND
+ * @create 2022-05-29 15:52
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello.git");
+    }
+}
