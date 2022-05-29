afe1fde2002dd9a5406a6ac690c82a4b870bcad4
BEYOND3361237546 <95428109+BEYOND3361237546@users.noreply.github.com>
2022-05-29 17:10:39 +0800

    new2

--- src/main/java/com/BEYOND/java/HelloGit.java
+++ src/main/java/com/BEYOND/java/HelloGit.java
@@ -7,21 +7,5 @@ package com.BEYOND.java;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello.git1");
-        System.out.println("Hello.git2");
-        System.out.println("Hello.git3");
-        System.out.println("Hello.git4");
-        System.out.println("master");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix");
     }
 }
