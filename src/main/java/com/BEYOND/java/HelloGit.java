f83f8abc35204e35438c3ae70f61dd49376d6bb3
BEYOND <3361237546@qq.com>
2022-05-29 16:02:06 +0800

    second commit

--- src/main/java/com/BEYOND/java/HelloGit.java
+++ src/main/java/com/BEYOND/java/HelloGit.java
@@ -6,6 +6,7 @@ package com.BEYOND.java;
  */
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello.git");
+        System.out.println("Hello.git1");
+        System.out.println("Hello.git2");
     }
 }
